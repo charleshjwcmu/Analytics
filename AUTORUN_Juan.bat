dfb043c2c8c4e81dd8095fbb20f4a271b28d7325
charleshjw <huangjiawei579@gmail.com>
2020-04-29 03:09:16 -0400

    updated version

--- AUTORUN_Juan.bat
+++
@@ -1,6 +0,0 @@
-echo "AUTORUN"
-
-C:\Users\e577568\AppData\Local\Continuum\anaconda3\python Z:\Charles\ORM\SourceCodes-Production\main.py >> Z:\Charles\ORM\LOG\Log_main_executed_by_Juan_%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~0,2%h_%time:~3,2%m_%time:~6,2%s.txt
-
-pause
-
